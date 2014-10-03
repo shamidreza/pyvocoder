c483f812a557682d9612b6c35cac7267070d443b
shamidreza <hamidreza.plus@gmail.com>
2014-10-03 15:10:21 -0700

    lsf2poly and poly2lsf

--- lpc.py
+++ lpc.py
@@ -12,14 +12,47 @@ from scipy.signal import lfilter, hamming, freqz
         
 def levinson_durbin():
     pass
+def poly2lsf(a):
+    a = a / a[0]        
+    A = np.r_[a, 0.0]
+    B = A[::-1]
+    P = A - B  
+    Q = A + B  
+    
+    P = deconvolve(P, np.array([1.0, -1.0]))[0]
+    Q = deconvolve(Q, np.array([1.0, 1.0]))[0]
+    
+    roots_P = np.roots(P)
+    roots_Q = np.roots(Q)
+    
+    angles_P = np.angle(roots_P[::2])
+    angles_Q = np.angle(roots_Q[::2])
+    angles_P[angles_P < 0.0] += np.pi
+    angles_Q[angles_Q < 0.0] += np.pi
+    lsf = np.sort(np.r_[angles_P, angles_Q])
+    return lsf
 
+def lsf2poly(L):
+    order = len(L)
+    Q = L[::2]
+    P = L[1::2]
+    poles_P = np.r_[np.exp(1j*P),np.exp(-1j*P)]
+    poles_Q = np.r_[np.exp(1j*Q),np.exp(-1j*Q)]
+    
+    P = np.poly(poles_P)
+    Q = np.poly(poles_Q)
+    
+    P = convolve(P, np.array([1.0, -1.0]))
+    Q = convolve(Q, np.array([1.0, 1.0]))
+    
+    a = 0.5*(P+Q)
+    return a[:-1]
 class LPCVocoder(SourceFilterVocoder):
     def __init__(self, order, fs):
         self.src = source.PulseNoiseSource(fs)
         self.filt = LPCFilter(order, fs)
     
 class LPCFilter(Filter):
-    
     def __init__(self, order, fs):
         Filter.__init__(self, order, fs)
    
@@ -30,6 +63,22 @@ class LPCFilter(Filter):
         return lfilter([1], param, src_signal)
     def filter2spectrum(self, param):
         return np.abs(freqz([1], param)[1])
+    def spectrum2filter():
+        pass   
+   
+class LSFFilter(LPCFilter):
+    def __init__(self, order, fs):
+        LPCFilter.__init__(self, order, fs)
+   
+    def _encode_frame(self, signal):
+        A = LPCFilter._encode_frame(self, signal)
+        return poly2lsf(A)
+    def _decode_frame(self, param, src_signal):
+        A = lsf2poly(param)
+        return LPCFilter._decode_frame(self, A)
+    def filter2spectrum(self, param):
+        A = lsf2poly(param)        
+        return LPCFilter.filter2spectrum(self, A)
     def spectrum2filter():
         pass    
  
